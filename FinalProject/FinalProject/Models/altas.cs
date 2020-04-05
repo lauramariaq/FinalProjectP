acaddd28d1e1fe214579470864dd3a8a0c9192d3
LauraMaria <lauraqh2009@hotmail.com>
2020-04-05 17:58:32 -0400

    add

--- FinalProject/FinalProject/Models/altas.cs
+++ FinalProject/FinalProject/Models/altas.cs
@@ -2,10 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace FinalProject.Models
 {
     public class altas
     {
+        [Required(ErrorMessage = "id required")]
+        public int id { get; set; }
+
+        [Required(ErrorMessage = "IDIngreso required")]
+        public int idIngreso { get; set; }
+
+        [Required(ErrorMessage = "monto required")]
+        public int  monto { get; set; }
+
     }
 }
