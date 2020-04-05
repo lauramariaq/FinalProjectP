b60db8e1289672ef3b2839f4e0e8820aaeefa4ef
LauraMaria <lauraqh2009@hotmail.com>
2020-04-05 16:32:45 -0400

    Empezamos el proyecto agregandole las vistas necesarias y creando las clases que utilizaremos para la creacion de la base de datos mediante EntutyFramework. Tambien actulizamos el archivo layout para personalizar el diseno de la pagina

---
+++ FinalProject/FinalProject/Models/doctor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace FinalProject.Models
+{
+    public class doctor
+    {
+        [Required(ErrorMessage = "ID required")]
+        public int ID { get; set; }
+
+        [Required(ErrorMessage = "cedula required")]
+        public string cedula { get; set; }
+
+        [Required(ErrorMessage = "Name required")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = " exequatur required")]
+        public String exequatur { get; set; }
+
+        [Required(ErrorMessage = " Especialidad required")]
+        public String Especialidad { get; set; }
+    
+    }
+}
---
+++ FinalProject/FinalProject/Models/patient.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+namespace FinalProject.Models
+{
+    public class patient
+    {
+        [Required(ErrorMessage = "ID required")]
+        public int ID { get; set; }
+
+        [Required(ErrorMessage = "cedula required")]
+        public string Cedula { get; set; }
+
+        [Required(ErrorMessage = "Name required")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "insurance required")]
+        public bool insurance { get; set; }
+    }
+}
